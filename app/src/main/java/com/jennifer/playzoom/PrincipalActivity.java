a3fe8b272e65ca3bf41826faf7fd6cb9c3550078
jennifer <jennifertorrespalomino@gmail.com>
2023-07-16 17:10:55 -0500

    Nueva actualización

--- app/src/main/java/com/jennifer/playzoom/PrincipalActivity.java
+++ app/src/main/java/com/jennifer/playzoom/PrincipalActivity.java
@@ -1,8 +1,11 @@
 package com.jennifer.playzoom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.jennifer.playzoom.databinding.ActivityPrincipalBinding;
@@ -20,8 +23,27 @@ public class PrincipalActivity extends AppCompatActivity {
         binding.fabAddMovie.setOnClickListener(v -> {
             Snackbar.make(binding.getRoot(), "Add a movie", Snackbar.LENGTH_SHORT).show();
         });
-        binding.tbPlayzoom.setTitle("Bienvenido(a) Jennifer");
         setSupportActionBar(binding.tbPlayzoom);
-
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.movie_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (item.getItemId() == R.id.search){
+            Snackbar.make(binding.getRoot(), "Search", Snackbar.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.settings) {
+            Snackbar.make(binding.getRoot(), "Settings", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
